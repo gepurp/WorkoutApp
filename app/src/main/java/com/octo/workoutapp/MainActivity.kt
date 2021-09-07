6b6741b32f8a76236cf766a03ab35e723e28d37a
pkonstantinov <pkonstantinov@tradingview.com>
2021-09-07 08:30:40 +0300

    Add image, colors and part of layout

--- app/src/main/java/com/octo/workoutapp/MainActivity.kt
+++ app/src/main/java/com/octo/workoutapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.octo.workoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.octo.workoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.llStartButton.setOnClickListener {
+            Toast.makeText(this, "TEST", Toast.LENGTH_SHORT).show()
+        }
     }
 }
